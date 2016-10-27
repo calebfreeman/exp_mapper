63384010342894ab141f5c8bd2343565a2da2941
Caleb Freeman <caleb.freeman@marketful.com>
2016-10-27 02:49:50 -0400

    added config file, writes directly to saved file, prompts for inputs, native villages set

--- generate_map.py
+++ generate_map.py
@@ -1,16 +1,27 @@
 import random
 from itertools import *
+import binascii
+import sys
+from config import save_dir
 
+testing = True
+
+filepath = save_dir + str(raw_input("Path & filename for saved game ('RANDOM'): ") or "RANDOM")
 # Initial variables
+reveal_map = str(raw_input('Reveal map for all players? (Yn):') or 'Y')
+reveal = False
+if reveal_map == 'Y' or reveal_map =='Yes' or reveal_map == 'yes' or reveal_map == 'y':
+	reveal = True
+
 x_len = 80
 y_len = 80
-land_perc = .75
+land_perc = float(raw_input('Land percentage (40): ') or 40)/100
 land_num_tiles = land_perc * x_len * y_len
-continents = 20
-rivers = (land_num_tiles * .7)/5
+continents = int(raw_input('Continent Seeds (3): ') or 3)
+rivers = int(raw_input('River Seeds (10): ') or 10)
 river_length = 2
 new_map = []
-reset_coastline_num = 3
+reset_coastline_num = 4
 land_tiles = []
 land_tile_options = []
 chosen_land_tiles = []
@@ -42,16 +53,8 @@ river_spawn_tiles = {
 	
 }
 
-river_spawn_tiles2 = {
-	'44' : '0D', #sea on left
-	'46' : '0E', #sea on bottom
-	'47' : '0F', #sea on right
-	'45' : '10' #sea on top
-}
-
 coastal_tiles = ['01','02','03','04','05','06','07','08','09','0A','0B','0C','0D','0E','0F','10','11','12','13','14','15','16','17','18','44','45','46','47']
-
-inland_tiles = []
+inland_tiles = ['19','1A','1B','1C','1D','1E','1F','26','27','28','29','2A','2F','30','3B']
 river_tiles = ['3D','3C','3E','3F','40','41','43','48','49','4A','4B'] 
 
 for each in range(river_length):
@@ -353,15 +356,111 @@ def extend_rivers():
 		del river_ext_pts[river_ext_pt_idx]
 		i += 1
 
+inland_rectangles = []
+def get_area():
+	random.randint(2,)
+
+inland_up_cols = []
+
+def find_inland_rows():
+	row = 0
+	inland_right_rows = []
+	for y in range(y_len):
+		col = 0
+		inland_right_rows.append([])
+		for x in range(x_len):
+			inland_right_rows[row].append(0)
+			if new_map[y][x] in inland_tiles:
+				inland_right_rows[row][col] = 1
+				valid = True
+				trace_x = x
+				while valid == True:
+					if trace_x == x_len-1:
+						trace_x = 0
+					else:
+						trace_x += 1
+					if new_map[y][trace_x] in inland_tiles:
+						inland_right_rows[row][col] += 1
+					else:
+						valid = False
+			col += 1
+		row += 1
+	#for r in inland_right_rows:
+	#	print r
+
+def find_inland_cols():
+	col = x_len-1
+	inland_up_cols = [[0 for x in xrange(x_len)] for x in xrange(y_len)]
+	while col >= 0:
+		row = y_len-1
+		while row >= 0:
+			if new_map[row][col] in inland_tiles:
+				inland_up_cols[row][col] = 1
+				valid = True
+				trace_y = row
+				while valid == True:
+					if trace_y == 0:
+						trace_y = y_len-1
+					else:
+						trace_y = trace_y-1
+					if new_map[trace_y][col] in inland_tiles:
+						inland_up_cols[row][col] += 1
+					else:
+						valid = False
+			row -= 1
+		col -= 1
+	#for r in inland_up_cols:
+	#	print r	
+
+def set_native_villages():
+	villages_layer = ''
+	villages = ['00','0D']
+	for y in range(y_len):
+		for x in range(x_len):
+			if new_map[y][x] in ['09','0A','0B','0C','0D','0E','0F','10','11','12','13','14','15','16','17','18','19','1A','1B']:
+				num = random.randint(0,1000)
+				if num < 100:
+					if num < 15:
+						villages_layer += '0D'
+					else:
+						villages_layer += '00'
+				else:
+					villages_layer += 'FF'
+			else:
+				villages_layer += 'FF'
+	if testing:
+		test_string = '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff'
+
+		villages_layer = test_string + villages_layer[512:]
+	return villages_layer 
+
+
+
 
 
 def write_file():
-	f = open("out.txt", "w")
+	f = open(filepath, "w+")
 	f.seek(0)
+	test_list = ['01','02','03','04','05','06','07','08','09','0a','0b','0c','0d','0e','0f','10','11','12','13','14','15','16','17','18','19','1a','1b','1c','1d','1e','1f','20','21','22','23','24','25','26','27','28','29','2a','2b','2c','2d','2e','2f','30','31','32','33','34','35','36','37','38','39','3a','3b','3c','3d','3e','3f','40','41','42','43','44','45','46','47','48','49','4a','4b','4c','4d','4e','4f','50','51','52','53','54','55','56','57','58','59','5a','5b','5c','5d','5e','5f','60','61','62','63','64','65','66','67','68','69','6a','6b','6c','6d','6e','6f','70','71','72','73','74','75', '76', '77', '78', '79', '7a', '7b', '7c', '7d', '7e', '7f', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '8a', '8b', '8c', '8d', '8e', '8f', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '9a', '9b', '9c', '9d', '9e', '9f', 'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'ba', 'bb', 'bc', 'bd', 'be', 'bf', 'c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'ca', 'cb', 'cc', 'cd', 'ce', 'cf', 'd0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'da', 'db', 'dc', 'dd', 'de', 'df', 'e0', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9', 'ea', 'eb', 'ec', 'ed', 'ee', 'ef', 'f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'fa', 'fb', 'fc', 'fd', 'fe', 'ff']
+	out = ''
+	r = 0
 	for map_row in new_map:
+		if r < 63 and testing:
+			map_row[0:4] = test_list[0:4]
+			del test_list[0:4]
 		for map_col in map_row:
-			f.write(map_col)
-	f.truncate()
+			out += map_col
+		r += 1
+
+	f.write(binascii.unhexlify(out))
+	if reveal:
+		f.write(binascii.unhexlify('1F'*6400))
+	else:
+		f.write(binascii.unhexlify('00'*6400))
+	f.write(binascii.unhexlify('FF'*6400))
+	f.write(binascii.unhexlify(villages))
+	f.write(open('TEMPLATE.SAV', "r").read())
+	#f.truncate()
 	f.close()
 
 print 'Creating map'
@@ -380,6 +479,11 @@ print "Spawning rivers"
 spawn_rivers()
 print "Extending rivers"
 extend_rivers()
+print "Creating mountains & valleys"
+find_inland_rows()
+find_inland_cols()
+print "Building native villages & incan cities"
+villages = set_native_villages()
 print "Writing to file"
 write_file()
 print 'Done.'
